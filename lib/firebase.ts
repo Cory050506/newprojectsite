555a5bf1ccfddfd5769e88b38ef684aa46802684
corytest <corkyhold@gmail.com>
2025-12-11 08:47:55 -0500

    Fix firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,17 @@
-// lib/firebase.ts
-import { initializeApp, getApps, getApp } from "firebase/app";
+// app/firebase.ts
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACvC_hSZUaqEDh1r9ohjUS2fZhr0iJXqY",
-  authDomain: "stockpilot-e2a28.firebaseapp.com",
-  projectId: "stockpilot-e2a28",
-  storageBucket: "stockpilot-e2a28.firebasestorage.app",
-  messagingSenderId: "1067461917580",
-  appId: "1:1067461917580:web:e70b5f8b465d4182e1f4a9",
-  measurementId: "G-L0PWL48295"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export default app;
